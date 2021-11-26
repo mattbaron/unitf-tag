ec935c61bb1aaa0133c0eea0cfe6b2374d3b8fd3
Matt Baron <mwb@unitf.net>
2021-11-26 15:00:22 -0600

    .

---
+++ lib/unitf/tag.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "tag/version"
+
+module Unitf
+  module Tag
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/unitf/tag_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Unitf::Tag do
+  it "has a version number" do
+    expect(Unitf::Tag::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
