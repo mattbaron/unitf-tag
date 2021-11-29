69808450ecb9699c4ca698b32038f175adf83215
Matt Baron <mwb@unitf.net>
2021-11-28 18:58:26 -0600

    .

---
+++ lib/unitf/tag/dumper.rb
@@ -0,0 +1,18 @@
+require 'json'
+
+module UnitF
+  module Tag
+    class Dumper
+      def initialize
+        @objects = []
+      end
+
+      def spool(object)
+      end
+
+      def dump
+
+      end
+    end
+  end
+end
